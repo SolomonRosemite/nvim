47c51783c102c5bb099562ee370b27b4e260e064
Solomon Rosemite™ <SolomonRosemiteProjects@gmail.com>
2023-07-28 10:31:53 +0200

    feat: add rainbow csv pkg

--- lua/rosemite/packer.lua
+++ lua/rosemite/packer.lua
@@ -43,6 +43,27 @@ return require('packer').startup(function(use)
 		}
 	}
 
+    use {
+        'cameron-wags/rainbow_csv.nvim',
+        config = function()
+            require 'rainbow_csv'.setup()
+        end,
+        -- optional lazy-loading below
+        module = {
+            'rainbow_csv',
+            'rainbow_csv.fns'
+        },
+        ft = {
+            'csv',
+            'tsv',
+            'csv_semicolon',
+            'csv_whitespace',
+            'csv_pipe',
+            'rfc_csv',
+            'rfc_semicolon'
+        }
+    }
+
 	use({ 'rose-pine/neovim', as = 'rose-pine' })
 	use({ 'catppuccin/nvim', as = 'catppuccin' })
 
