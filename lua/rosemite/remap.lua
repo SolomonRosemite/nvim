f720e503175629e4590bf435922387a0e359419b
Solomon Rosemite™ <SolomonRosemiteProjects@gmail.com>
2023-08-25 10:45:23 +0200

    feat: add copy to clipboard remap

--- lua/rosemite/remap.lua
+++ lua/rosemite/remap.lua
@@ -3,6 +3,7 @@ vim.keymap.set("n", "<leader>e", vim.cmd.Ex)
 
 vim.api.nvim_set_keymap('n', '<C-d>', '<C-d>zz', {noremap = true, silent = true})
 vim.api.nvim_set_keymap('n', '<C-u>', '<C-u>zz', {noremap = true, silent = true})
+vim.api.nvim_set_keymap('n', '<C-c>', ':silent exe \"\'<,\'>w !clip.exe\"<cr>', { noremap = true, silent = true })
 
 vim.keymap.set("v", "J", ":m '>+1<CR>gv=gv")
 vim.keymap.set("v", "K", ":m '<-2<CR>gv=gv")
