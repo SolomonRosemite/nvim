e4fc43ea20db9536d73c717cb8c2f5ccd6cb7b3e
Solomon Rosemite™ <SolomonRosemiteProjects@gmail.com>
2023-09-25 17:34:33 +0200

    feat: enable copying to clipboard from WSL

--- lua/rosemite/remap.lua
+++ lua/rosemite/remap.lua
@@ -3,7 +3,6 @@ vim.keymap.set("n", "<leader>e", vim.cmd.Ex)
 
 vim.api.nvim_set_keymap('n', '<C-d>', '<C-d>zz', {noremap = true, silent = true})
 vim.api.nvim_set_keymap('n', '<C-u>', '<C-u>zz', {noremap = true, silent = true})
-vim.api.nvim_set_keymap('n', '<C-c>', ':silent exe \"\'<,\'>w !clip.exe\"<cr>', { noremap = true, silent = true })
 
 vim.keymap.set("v", "J", ":m '>+1<CR>gv=gv")
 vim.keymap.set("v", "K", ":m '<-2<CR>gv=gv")
@@ -15,3 +14,22 @@ vim.keymap.set('n', '<leader>P', '"0P')
 
 vim.keymap.set('n', 'K', '<C-w>k')
 vim.keymap.set('n', 'J', '<C-w>j')
+
+-- Copy to clipboard from WSL
+vim.keymap.set('v', '<C-c>', '"+y')
+-- vim.keymap.set('n', '<C-c>', '"+y')
+
+-- Copy to clipboard from WSL
+vim.g.clipboard = {
+    name = 'WslClipboard',
+    copy = {
+        ["+"] = 'clip.exe',
+        ["*"] = 'clip.exe',
+    },
+    paste = {
+        ["+"] = 'powershell.exe -c [Console]::Out.Write($(Get-Clipboard -Raw).tostring().replace("`r", ""))',
+        ["*"] = 'powershell.exe -c [Console]::Out.Write($(Get-Clipboard -Raw).tostring().replace("`r", ""))',
+    },
+    cache_enabled = 0,
+}
+
