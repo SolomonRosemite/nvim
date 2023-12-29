2401440379f1cde3a6ddb397c5fbf458e4075c50
Solomon Rosemite™ <SolomonRosemiteProjects@gmail.com>
2023-12-29 10:14:15 +0100

    feat: add remap for telescope live grep

--- after/plugin/telescope.lua
+++ after/plugin/telescope.lua
@@ -1,8 +1,7 @@
 local builtin = require('telescope.builtin')
 vim.keymap.set('n', '<C-p>', builtin.find_files, {})
-vim.keymap.set('n', '<leader>grep', function()
-    builtin.grep_string({ search = vim.fn.input("Grep > ") })
-end)
+vim.keymap.set('n', '<leader>grep', builtin.live_grep, {})
+-- TODO: add marks
 
 require('telescope').setup{
   defaults = {
