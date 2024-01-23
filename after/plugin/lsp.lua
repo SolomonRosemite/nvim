a43d563093d32840031b14fef911fccbd4dccdd5
Solomon Rosemite™ <SolomonRosemiteProjects@gmail.com>
2024-01-23 09:56:47 +0100

    feat: update and use telescope lsp references

--- after/plugin/lsp.lua
+++ after/plugin/lsp.lua
@@ -62,7 +62,7 @@ lsp.on_attach(function(client, bufnr)
   vim.keymap.set("n", "[d", function() vim.diagnostic.goto_next() end, opts)
   vim.keymap.set("n", "]d", function() vim.diagnostic.goto_prev() end, opts)
   vim.keymap.set("n", "<leader>ca", function() vim.lsp.buf.code_action() end, opts)
-  vim.keymap.set("n", "<leader>gr", function() vim.lsp.buf.references() end, opts)
+  -- vim.keymap.set("n", "<leader>gr", function() vim.lsp.buf.references() end, opts)
   vim.keymap.set("i", "<C-h>", function() vim.lsp.buf.signature_help() end, opts)
 
   lsp.buffer_autoformat()
--- after/plugin/telescope.lua
+++ after/plugin/telescope.lua
@@ -1,13 +1,47 @@
 local builtin = require('telescope.builtin')
 vim.keymap.set('n', '<C-p>', builtin.find_files, {})
-vim.keymap.set('n', '<leader>grep', builtin.live_grep, {})
+vim.keymap.set('n', '<C-k>', builtin.live_grep, {})
+vim.keymap.set('n', 'gr', builtin.lsp_references, {})
 -- TODO: add marks
 
 require('telescope').setup{
-  defaults = {
-    file_ignore_patterns = {
-        "dist",
+    pickers = {
+        live_grep = {
+            hidden = true,
+            no_ignore = true,
+            find_command = {
+                'rg',
+                '--files',
+                '--color=never',
+                '--no-heading',
+                '--line-number',
+                '--column',
+                '--smart-case',
+                '--hidden',
+                '--glob',
+                '!{**/.git/*,**/.svelte-kit/*,**/target/*,**/node_modules/*,**/.dist/*}',
+                '--sort',
+                'path',  -- Add this line to order by directory
+            },
+        },
+        find_files = {
+            hidden = true,
+            no_ignore = true,
+            find_command = {
+                'rg',
+                '--files',
+                '--color=never',
+                '--no-heading',
+                '--line-number',
+                '--column',
+                '--smart-case',
+                '--hidden',
+                '--glob',
+                '!{**/.git/*,**/.svelte-kit/*,**/target/*,**/node_modules/*,**/.dist/*}',
+                '--sort',
+                'path',  -- Add this line to order by directory
+            },
+        },
     }
-  }
 }
 
