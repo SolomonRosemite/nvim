b1d7b4d01dc6d781ce84522c391552816415f5cf
Solomon Rosemite™ <SolomonRosemiteProjects@gmail.com>
2024-03-14 11:18:57 +0100

    feat: add fugitive aliases

--- lua/plugins/fugitive.lua
+++ lua/plugins/fugitive.lua
@@ -2,5 +2,9 @@ return {
   'tpope/vim-fugitive',
   config = function()
     vim.keymap.set('n', '<leader>gs', vim.cmd.Git)
+    vim.keymap.set('n', '<leader>gd', ':Git diff<CR>', { noremap = true, silent = true })
+    vim.keymap.set('n', '<leader>gc', ':Git commit<CR>', { noremap = true, silent = true })
+    vim.keymap.set('n', '<leader>gb', ':Git blame<CR>', { noremap = true, silent = true })
+    vim.keymap.set('n', '<leader>gl', ':Git log<CR>', { noremap = true, silent = true })
   end,
 }
