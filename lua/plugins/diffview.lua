cc170146f6bf03b6f8ffb60d099706a81fa743f6
Solomon Rosemite™ <SolomonRosemiteProjects@gmail.com>
2024-03-14 18:32:02 +0100

    feat: add disable ctrl L remap for diffview

--- lua/plugins/diffview.lua
+++ lua/plugins/diffview.lua
@@ -1,1 +1,14 @@
-return { 'sindrets/diffview.nvim' }
+return {
+  'sindrets/diffview.nvim',
+  config = function()
+    require('diffview').setup {
+      diff_binaries = false, -- Show diffs for binaries
+      keymaps = {
+        disable_defaults = false, -- Disable the default keymaps
+        file_panel = {
+          { 'n', 'L' },
+        },
+      },
+    }
+  end,
+}
