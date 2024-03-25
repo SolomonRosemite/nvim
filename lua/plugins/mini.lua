ef806f363de01fc4dbdd798ee5223c2c581d6cee
Solomon Rosemite™ <SolomonRosemiteProjects@gmail.com>
2024-03-25 22:55:58 +0100

    feat: remove mini statusline & remove mini surround

--- lua/plugins/mini.lua
+++ lua/plugins/mini.lua
@@ -14,23 +14,8 @@ return { -- Collection of various small independent plugins/modules
     -- - saiw) - [S]urround [A]dd [I]nner [W]ord [)]Paren
     -- - sd'   - [S]urround [D]elete [']quotes
     -- - sr)'  - [S]urround [R]eplace [)] [']
-    require('mini.surround').setup()
+    -- require('mini.surround').setup() -- TODO: messes with leap "s" key
 
-    -- Simple and easy statusline.
-    --  You could remove this setup call if you don't like it,
-    --  and try some other statusline plugin
-    local statusline = require 'mini.statusline'
-    statusline.setup()
-
-    -- You can configure sections in the statusline by overriding their
-    -- default behavior. For example, here we disable the section for
-    -- cursor information because line numbers are already enabled
-    ---@diagnostic disable-next-line: duplicate-set-field
-    statusline.section_location = function()
-      return ''
-    end
-
-    -- ... and there is more!
     --  Check out: https://github.com/echasnovski/mini.nvim
   end,
 }
