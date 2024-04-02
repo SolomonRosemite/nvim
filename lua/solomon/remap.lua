267f7d11d520a15df40f3af09aa5164d4a78564c
Solomon Rosemite™ <SolomonRosemiteProjects@gmail.com>
2024-04-02 20:18:20 +0200

    feat: remap find replace keymap

--- lua/solomon/remap.lua
+++ lua/solomon/remap.lua
@@ -33,7 +33,7 @@ vim.keymap.set('n', '<C-Left>', '<C-w><<C-w><')
 
 vim.keymap.set('n', '<C-q>', ':q<CR>')
 
-vim.keymap.set('n', '<leader>rn', ':%s/f/r/g')
+vim.keymap.set('n', '<leader>fr', ':%s/f/r/g')
 
 vim.api.nvim_set_keymap('n', '<leader>cc', ':noh<CR>', { noremap = true, silent = true })
 
