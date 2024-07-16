11747ec91d15cbbaa73d925ee93f9e29eb46b8c7
Solomon Rosemite™ <SolomonRosemiteProjects@gmail.com>
2024-07-16 16:59:46 +0200

    refactor: restructure handling colorschemes

---
+++ after/plugin/select-colors.lua
@@ -0,0 +1,44 @@
+function Transparent(color)
+  vim.cmd.colorscheme(color)
+  vim.api.nvim_set_hl(0, 'Normal', { bg = 'none' })
+  vim.api.nvim_set_hl(0, 'NormalFloat', { bg = 'none' })
+end
+
+-- vim.opt.background = 'dark'
+-- vim.cmd 'colorscheme oxocarbon'
+
+-- vim.cmd.colorscheme 'tokyonight-night'
+-- vim.cmd.hi 'Comment gui=none'
+
+require('catppuccin').setup {
+  transparent_background = true,
+}
+vim.cmd.colorscheme 'catppuccin-mocha'
+
+-- vim.cmd 'colorscheme rose-pine'
+-- Transparent 'rose-pine'
+
+require('onedark').setup {
+  transparent = true,
+  style = 'darker',
+  lualine = {
+    transparent = true,
+  },
+}
+-- require('onedark').load()
+
+local p = require 'poimandres.palette'
+require('poimandres').setup {
+  dim_nc_background = true, -- dim 'non-current' window backgrounds
+  disable_background = true, -- disable background
+  -- disable_float_background = true, -- disable background for floats
+  bold_vert_split = false, -- use bold vertical separators
+  disable_italics = false, -- disable italics
+  -- fix for: https://github.com/olivercederborg/poimandres.nvim/issues/44#issuecomment-2182983874
+  highlight_groups = {
+    LspReferenceText = { bg = p.background1 },
+    LspReferenceRead = { bg = p.background1 },
+    LspReferenceWrite = { bg = p.background1 },
+  },
+}
+-- vim.cmd 'colorscheme poimandres'
--- lua/plugins/colors.lua
+++ lua/plugins/colors.lua
@@ -1,62 +1,29 @@
-function Transparent(color)
-  vim.cmd.colorscheme(color)
-  vim.api.nvim_set_hl(0, 'Normal', { bg = 'none' })
-  vim.api.nvim_set_hl(0, 'NormalFloat', { bg = 'none' })
-end
-
 return {
   {
     'folke/tokyonight.nvim',
-    lazy = false,
-    config = function()
-      vim.opt.background = 'dark'
-      vim.cmd 'colorscheme oxocarbon'
-
-      -- vim.cmd.colorscheme 'tokyonight-night'
-      -- vim.cmd.hi 'Comment gui=none'
-
-      -- require('catppuccin').setup {
-      --   transparent_background = true,
-      -- }
-      --
-      -- vim.cmd.colorscheme 'catppuccin-mocha'
-
-      -- vim.cmd 'colorscheme rose-pine'
-      -- Transparent 'rose-pine'
-
-      require('onedark').setup {
-        transparent = true,
-        style = 'darker',
-        lualine = {
-          transparent = true,
-        },
-      }
-      require('onedark').load()
-
-      -- require('poimandres').setup {}
-      -- vim.cmd 'colorscheme poimandres'
-    end,
+    lazy = true,
   },
   {
     'nyoom-engineering/oxocarbon.nvim',
-    lazy = false,
+    lazy = true,
   },
   {
     'rose-pine/neovim',
     name = 'rose-pine',
-    lazy = false,
+    lazy = true,
   },
   {
     'navarasu/onedark.nvim',
-    lazy = false,
+    lazy = true,
   },
   {
     'catppuccin/nvim',
     name = 'catppuccin',
-    lazy = false,
+    lazy = true,
   },
   {
     'olivercederborg/poimandres.nvim',
     lazy = false,
+    priority = 1000,
   },
 }
