a15f97027b2a6ffe6548a2c1e0940569e5364551
Solomon Rosemite™ <SolomonRosemiteProjects@gmail.com>
2024-09-15 19:53:13 +0200

    feat: add new harpoon config

--- lua/plugins/harpoon.lua
+++ lua/plugins/harpoon.lua
@@ -1,19 +1,69 @@
 return {
   'ThePrimeagen/harpoon',
+  branch = 'harpoon2',
   dependencies = { 'nvim-lua/plenary.nvim' },
   cond = vim.g.vscode == nil,
+  opts = { settings = {
+    save_on_toggle = false,
+    sync_on_ui_close = false,
+  } },
   config = function()
-    local hpm = require 'harpoon.mark'
-    local hpu = require 'harpoon.ui'
+    local harpoon = require 'harpoon'
+    local Config = require 'harpoon.config'
+    local Path = require 'plenary.path'
 
-    vim.keymap.set('n', '<leader>a', hpm.add_file, {})
-    vim.keymap.set('n', '<leader>q', hpu.toggle_quick_menu, {})
-    vim.keymap.set('n', '<leader>h', hpu.toggle_quick_menu, {})
+    vim.keymap.set('n', '<leader>A', function()
+      harpoon:list('my_custom_list'):add()
+    end)
+
+    vim.keymap.set('n', '<leader>a', function()
+      harpoon:list('my_custom_list'):add(Config.get_default_config().default.create_list_item(Config.get_default_config().default))
+    end)
+
+    vim.keymap.set('n', '<leader>q', function()
+      harpoon.ui:toggle_quick_menu(harpoon:list 'my_custom_list')
+    end)
 
     for i = 1, 9 do
       vim.keymap.set('n', '<leader>' .. i, function()
-        hpu.nav_file(i)
+        harpoon:list('my_custom_list'):select(i)
       end, {})
     end
+
+    harpoon:setup {
+      settings = {
+        save_on_toggle = true,
+        sync_on_ui_close = true,
+      },
+      my_custom_list = {
+        create_list_item = function(config, name)
+          name = name or Path:new(vim.api.nvim_buf_get_name(vim.api.nvim_get_current_buf())):make_relative(config.get_root_dir())
+          local bufnr = vim.fn.bufnr(name, false)
+          local pos = { 1, 0 }
+          if bufnr ~= -1 then
+            pos = vim.api.nvim_win_get_cursor(vim.api.nvim_get_current_win())
+          end
+
+          return {
+            value = string.format('%s: (%d,%d)', name, pos[1], pos[2]),
+            context = {
+              custom = true,
+            },
+          }
+        end,
+        select = function(list_item, list, options)
+          if list_item.context.custom == nil then
+            Config.get_default_config().default.select(list_item, list, options)
+            return
+          end
+
+          local name, row, col = list_item.value:match '(.+): %((%d+),(%d+)%)'
+          Config.get_default_config().default.select({ value = name, context = { row = tonumber(row), col = tonumber(col) } }, list, options)
+
+          local win = vim.api.nvim_get_current_win()
+          vim.api.nvim_win_set_cursor(win, { tonumber(row), tonumber(col) })
+        end,
+      },
+    }
   end,
 }
